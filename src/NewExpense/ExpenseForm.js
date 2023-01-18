e3538962da400ca19833cff641026928899fa70c
Birje <birje.solatte@voco.ee>
2023-01-18 16:09:35 +0200

    Added CSS

--- src/NewExpense/ExpenseForm.js
+++ src/NewExpense/ExpenseForm.js
@@ -1,27 +1,27 @@
 import React from 'react';
+import "./ExpenseForm.css";
 
 const ExpenseForm = (props) => {
     return (
         <form>
-            <div>
-                <div>
+            <div className="new-expense__control input">
+                <div className='new-expense__control'>
                     <label>Title</label>
                     <input type = 'text' />
                 </div>
-                <div>
+                <div className='new-expense__control'>
                     <label>Amount</label>
                     <input type = 'number' min='0.01' step='0.01'/>
                 </div>
-                <div>
+                <div className='new-expense__control'>
                     <label>Date</label>
                     <input type = 'date' min='2023-01-08' max='2025-12-31'/>
                 </div>
             </div>
-            <div>
-                <button type = 'submit'> Add Expense4</button>
+            <div className='new-expense__actions'>
+                <button type = 'submit'> Add Expense</button>
             </div>
         </form>
     )
 }
-
 export default ExpenseForm
--- src/NewExpense/NewExpense.js
+++ src/NewExpense/NewExpense.js
@@ -1,8 +1,12 @@
 import React from 'react';
+import ExpenseForm from "./ExpenseForm";
+import "./NewExpense.css"
 
 const NewExpense = () => {
     return (
-        <div></div>
+        <div className='new-expense' >
+            <ExpenseForm></ExpenseForm>
+        </div>
     )
 }
 
